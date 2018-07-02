3f57dcbadab6a2ca092722283d77ce9847d94b2a
Ricardo Dias <cadu.hd@gmail.com>
2018-07-02 20:31:36 -0300

    add trail to fast projectile

--- 01- Combat/Dragon RPG/Assets/Camera & UI/PlayerHealthBar.cs
+++ 01- Combat/Dragon RPG/Assets/Camera & UI/PlayerHealthBar.cs
@@ -1,12 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(RawImage))]
 public class PlayerHealthBar : MonoBehaviour
 {
-
     RawImage healthBarRawImage;
     Player player;
 
