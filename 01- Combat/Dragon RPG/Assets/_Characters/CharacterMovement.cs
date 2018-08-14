f8a3003485548a20e9fbf33221a70aa046aa40e1
Ricardo Dias <cadu.hd@gmail.com>
2018-08-13 23:36:55 -0300

    readd OnAnimatorMove method

--- 01- Combat/Dragon RPG/Assets/_Characters/CharacterMovement.cs
+++ 01- Combat/Dragon RPG/Assets/_Characters/CharacterMovement.cs
@@ -9,10 +9,13 @@ namespace RPG.Characters
     public class CharacterMovement : MonoBehaviour
     {
         [SerializeField] float stoppingDistance = 1f;
+        [SerializeField] float moveSpeedMultiplier = .7f;
         ThirdPersonCharacter character;
 
         GameObject walkTarget;
         NavMeshAgent agent;
+        Animator animator;
+        Rigidbody rigidBody;
 
         private void Start()
         {
@@ -26,6 +29,10 @@ namespace RPG.Characters
             agent.updatePosition = true;
 
             agent.stoppingDistance = stoppingDistance;
+
+            animator = GetComponent<Animator>();
+
+            rigidBody = GetComponent<Rigidbody>();
 
             cameraRaycaster.onMouseOverPotentiallyWalkable += OnMouseOverPotentiallyWalkable;
             cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
@@ -65,6 +72,23 @@ namespace RPG.Characters
         {
             Vector3 reductionVector = (destination - transform.position).normalized * shortening;
             return destination - reductionVector;
+        }
+
+        // adjust the speed of the root motion, of the central gravity of the character
+        void OnAnimatorMove()
+        {
+            // we implement this function to override the default root motio.
+            // This allow us to modify the positional speed before it's applied.
+            if (Time.deltaTime > 0)
+            {
+                // animator.deltaPosition - asking unity, what are you saying the change in position of the animator should be
+                // the positon multiplied by the move speed multiplier divided by the time (distance over time is equal speed)
+                Vector3 velocity = (animator.deltaPosition * moveSpeedMultiplier) / Time.deltaTime;
+
+                // we preserve the existing y part of the current velocity
+                velocity.y = rigidBody.velocity.y;
+                rigidBody.velocity = velocity;
+            }
         }
     }
 }
