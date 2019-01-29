a0d529b19174da98da62cb0a65a13b03b96dc4e5
Ricardo Dias <cadu.hd@gmail.com>
2019-01-29 07:55:18 -0200

    proj update

--- 01- Combat/Dragon RPG/Assets/UB/SimpleCS/Models/EmptyDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UB.SimpleCS.Models
-{
-    /// <summary>
-    /// Just a helper empty class to check network responses
-    /// </summary>
-    public class EmptyDto : BaseDto
-    {
-    }
-}
